4ba04096b013bdc02c332096c9d33c79e1d8c9a6
gouthamsiddi <123220434+gouthamsiddi@users.noreply.github.com>
2023-02-25 18:41:37 -0600

    Initial commit

--- src/hp_4.py
+++ src/hp_4.py
@@ -3,32 +3,60 @@
 from datetime import datetime, timedelta
 from csv import DictReader, DictWriter
 from collections import defaultdict
+import csv
 
 
-def reformat_dates(old_dates):
+def reformat_dates(dates):
     """Accepts a list of date strings in format yyyy-mm-dd, re-formats each
     element to a format dd mmm yyyy--01 Jan 2001."""
-    pass
+    new_dates = []
+    for date in dates:
+        datetime_obj = datetime.strptime(date, '%Y-%m-%d')
+        new_dates.append(datetime_obj.strftime('%d %b %Y'))
+    return new_dates
 
 
 def date_range(start, n):
     """For input date string `start`, with format 'yyyy-mm-dd', returns
     a list of of `n` datetime objects starting at `start` where each
     element in the list is one day after the previous."""
-    pass
+    if not isinstance(start, str):
+        raise TypeError('Start date must be a string')
+    if not isinstance(n, int):
+        raise TypeError('n must be an integer')
+    start_date = datetime.strptime(start, '%Y-%m-%d')
+    date_list = [start_date + timedelta(days=i) for i in range(n)]
+    return date_list
 
 
 def add_date_range(values, start_date):
     """Adds a daily date range to the list `values` beginning with
     `start_date`.  The date, value pairs are returned as tuples
     in the returned list."""
-    pass
+    dates = date_range(start_date, len(values))
+    return list(zip(dates, values))
+
 
 
 def fees_report(infile, outfile):
     """Calculates late fees per patron id and writes a summary report to
     outfile."""
-    pass
+    fees = {}
+    with open(infile, newline='') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            due_date = datetime.strptime(row['date_due'], '%m/%d/%Y')
+            return_date = datetime.strptime(row['date_returned'], '%m/%d/%Y')
+            if return_date > due_date:
+                days_late = (return_date - due_date).days
+                late_fee = days_late * 0.25
+                fees[row['patron_id']] = fees.get(row['patron_id'], 0) + late_fee
+    with open(outfile, 'w', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(['patron_id', 'late_fees'])
+        for patron_id, fee in fees.items():
+            writer.writerow([patron_id, round(fee, 2)])
+
 
 
 # The following main selection block will only run when you choose
@@ -45,8 +73,8 @@ if __name__ == '__main__':
     except ImportError:
         from util import get_data_file_path
 
-    # BOOK_RETURNS_PATH = get_data_file_path('book_returns.csv')
-    BOOK_RETURNS_PATH = get_data_file_path('book_returns_short.csv')
+    BOOK_RETURNS_PATH = get_data_file_path('book_returns.csv')
+    #BOOK_RETURNS_PATH = get_data_file_path('book_returns_short.csv')
 
     OUTFILE = 'book_fees.csv'
 
